5daeb10ebdfc3a062aaaee953df285b065ea1560
Erik Steiger <rkstgr@gmail.com>
2023-04-20 11:12:47 +0200

    Fix cifar10 dataset

--- dataset/cifar10.py
+++ dataset/cifar10.py
@@ -1,4 +1,6 @@
 import pickle
+
+import einops
 import numpy as np
 import torch
 import yaml
@@ -25,13 +27,12 @@ class Cifar10Dataset(Dataset):
             cifar_10_batch_dir,
             image_size,
             augment_horizontal_flip=False,
-            convert_image_to=None,
             split='train'
     ):
         super().__init__()
         self.root_dir = cifar_10_batch_dir
         self.image_size = image_size
-        self.image_shape = (3, 32, 32)  # cifar10 image shape
+        self.image_shape = (32, 32, 3)  # cifar10 image shape
         self.num_classes = 10  # cifar10 has 10 classes
 
         # load the data batches and labels
@@ -42,21 +43,19 @@ class Cifar10Dataset(Dataset):
             print('Warning: image size is bigger than 32x32. Images will interpolated to the given size.')
 
         self.transform = T.Compose([
+            T.Lambda(lambda x: einops.rearrange(x, 'c h w -> h w c')),
             T.ToPILImage(),
             T.Resize(self.image_size) if self.image_size != 32 else Identity(),
             T.RandomHorizontalFlip(p=1.0) if augment_horizontal_flip else Identity(),
             # T.RandomApply([T.ColorJitter(0.8, 0.8, 0.8, 0.2)], p=0.2) if split == 'train' else Identity(),
             T.ToTensor(),
-            T.ConvertImageDtype(convert_image_to) if convert_image_to is not None else Identity(),
-            T.Normalize((0.4915, 0.4823, 0.4468), (0.247, 0.2435, 0.2616))
-            # normalize using cifar-10 dataset mean and std
         ])
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, index):
-        img = self.data[index]
+        img = self.data[index]  # [3, 32, 32]
         label = self.labels[index]
         return self.transform(img)  # , label  # [3, 32, 32]
 
@@ -79,7 +78,9 @@ class Cifar10Dataset(Dataset):
                 labels += batch_data[b'labels']
 
         # concatenate the data batches and reshape to the cifar10 image shape
-        data = np.concatenate(data, axis=0).reshape(-1, *self.image_shape[::-1])
+        # data -- a 50000x3072 numpy array of uint8s. Each row of the array stores a 32x32 colour image. The first 1024 entries contain the red channel values, the next 1024 the green, and the final 1024 the blue. The image is stored in row-major order, so that the first 32 entries of the array are the red channel values of the first row of the image.
+        data = np.concatenate(data, axis=0)
+        data = data.reshape((data.shape[0], 3, 32, 32))
 
         return data, labels
 
